29ed56e8878ef17facf60d6d02d29ccb154bd828
Valentino Sandoval <u20211a962@upc.edu.pe>
2025-12-05 12:32:54 -0500

    feat: update ocelot configuration to use HTTPS and remove Consul integration

--- ApiGateway/ApiGateway/Program.cs
+++ ApiGateway/ApiGateway/Program.cs
@@ -1,31 +1,27 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using Ocelot.Provider.Consul;
 
 var builder = WebApplication.CreateBuilder(args);
 
-const string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-        builder =>
+    options.AddPolicy("_myAllowSpecificOrigins",
+        policy =>
         {
-            builder.WithOrigins("http://localhost:5173", "http://localhost:3000", "http://localhost:4200") 
-                .AllowAnyHeader() 
-                .AllowAnyMethod() 
-                .AllowCredentials(); 
+            policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
         });
 });
 
-
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-builder.Services.AddOcelot().AddConsul();
+
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
-app.UseCors(MyAllowSpecificOrigins);
+app.UseCors("_myAllowSpecificOrigins");
 
 await app.UseOcelot();
 
